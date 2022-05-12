078864d74426f5af798bccb286646ae1c9fffac1
Mahiuha <tiasho.mahiuha@gmail.com>
2022-05-12 23:35:53 +0300

    Encrypting passwords and checking valid passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" 5. Encrypting passwords
+"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ Salted pass generation
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ is valid?
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
